38f4e3d3233ca9ab04a33c4879e5176a2a013ff8
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-07 21:56:52 -0400

    react sin dolor 1

--- miproyecto/src/App.js
+++ miproyecto/src/App.js
@@ -1,13 +1,14 @@
 
-import './miCss.css';
+import './App.css';
 import imagen from './condorito.png';
 
 function App() {
-    let nombre = "Manchas";
+    let nombre = {nombre:"Manchas",
+                    edad: 7};
     return (
         <div className="fondo">
             <img src = {imagen} className = "condoro" alt="condorito"/>
-            <h1>Hola {nombre}</h1> 
+            <h1>Edad = {nombre.edad}</h1> 
         </div>
     );}
 
