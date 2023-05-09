6188934deff72b8884eff1704ede206b47c7c832
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-09 16:37:41 -0400

    react eventos funsion convertir

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,25 +1,39 @@
 
-import img_rey_1 from "./img/rey_sisebuto.png"
-import img_rey_2 from "./img/rey_atanagildo.png"
+import img_rey_1 from "./img/rey_sisebuto.png";
+//import img_rey_2 from "./img/rey_atanagildo.png";
+
+import {useRef} from "react";
 
 import './App.css';
 
 function App() {
 
+    const cambio = 23.16;
+    const refCaja = useRef();
+
     function incrementar(e)  {
-        e.target.innerHTML++;
-        e.target.style.backgroundColor = "red";
+        e.target.innerHTML++; 
+    }
+
+    function convertir() {
+        refCaja.current.innerHTML *= cambio;        
     }
 
     return (
         <>
-            <div onClick={incrementar} className="caja" >
+            <div ref={refCaja}
+                 className="caja"
+                 onClick={incrementar}
+            >
                 1
             </div>
-            <button>Aceptar</button>
+
+            <button onClick={convertir}>Aceptar</button>
+            
             <div>
                 <img src = {img_rey_1} alt="#"/> 
             </div>
+            
             <input className="cmpo"/>
         </>    
     );
