2ccc5444ba74b49eb93bff41956a023bfaee2f81
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-09 12:59:31 -0400

    react eventos-img

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-          </div>
-  );
+    return (
+        <>
+            <div className = "caja"></div>
+            <button>Aceptar</button>
+            <img />
+            <input />
+        </>    
+    );
 }
 
 export default App;
