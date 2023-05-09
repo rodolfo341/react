4d28c29c36c2fc8034c450bdf3058cada74a3442
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-09 16:15:58 -0400

    react eventos funcion incrementar incrementar

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -5,11 +5,21 @@ import img_rey_2 from "./img/rey_atanagildo.png"
 import './App.css';
 
 function App() {
+
+    function incrementar(e)  {
+        e.target.innerHTML++;
+        e.target.style.backgroundColor = "red";
+    }
+
     return (
         <>
-            <div className = "caja"></div>
+            <div onClick={incrementar} className="caja" >
+                1
+            </div>
             <button>Aceptar</button>
-            <img src = {img_rey_1} alt="#"/> 
+            <div>
+                <img src = {img_rey_1} alt="#"/> 
+            </div>
             <input className="cmpo"/>
         </>    
     );
