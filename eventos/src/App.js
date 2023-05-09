7c7a0b12358a09b540067827c14c5b803abb6229
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-09 12:59:41 -0400

    react eventos-img

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,4 +1,7 @@
 
+import img_rey_1 from "./img/rey_sisebuto.png"
+import img_rey_2 from "./img/rey_atanagildo.png"
+
 import './App.css';
 
 function App() {
@@ -6,8 +9,8 @@ function App() {
         <>
             <div className = "caja"></div>
             <button>Aceptar</button>
-            <img />
-            <input />
+            <img src = {img_rey_1} alt="#"/> 
+            <input className="cmpo"/>
         </>    
     );
 }
