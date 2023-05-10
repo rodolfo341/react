0118f6df44eae75cd9914572922940934ad8dd32
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-09 20:44:47 -0400

    react eventos Cambiar imagen del rey

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,22 +1,26 @@
 
 import img_rey_1 from "./img/rey_sisebuto.png";
-//import img_rey_2 from "./img/rey_atanagildo.png";
+import img_rey_2 from "./img/rey_atanagildo.png";
 
 import {useRef} from "react";
-
 import './App.css';
 
 function App() {
 
     const cambio = 23.16;
     const refCaja = useRef();
+    const refRey = useRef();
+    
+    function convertir() {
+        refCaja.current.innerHTML *= cambio;        
+    }
 
     function incrementar(e)  {
         e.target.innerHTML++; 
     }
 
-    function convertir() {
-        refCaja.current.innerHTML *= cambio;        
+    function cambiarRey (e) {
+        e.target.src = img_rey_2;
     }
 
     return (
@@ -30,8 +34,8 @@ function App() {
 
             <button onClick={convertir}>Aceptar</button>
             
-            <div>
-                <img src = {img_rey_1} alt="#"/> 
+            <div ref={refRey}>
+                <img onClick={cambiarRey} src = {img_rey_1} alt="#"/> 
             </div>
             
             <input className="cmpo"/>
