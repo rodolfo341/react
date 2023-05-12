7aef68f2ddda5c72951796c940a6e4befccaa05a
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-11 21:47:50 -0400

    react miproyecto

--- miproyecto/src/App.js
+++ miproyecto/src/App.js
@@ -1,26 +1,36 @@
 
 import './App.css';
 
-import imagen_1 from "./rey_sisebuto.png";
-import imagen_2 from './rey_leogivildo.png';
-import imagen_3 from './rey_atanagildo.png';
+import imagen_1 from "./img/rey_sisebuto.png";
+import imagen_2 from './img/rey_leogivildo.png';
+import imagen_3 from './img/rey_atanagildo.png';
+import imagen_ncognito from './img/rey_incognito.png';
+
+import {useRef} from "react";
 
 function App() {
     
+    const refFondo = useRef();
+
+    const incognito =(e)=> {
+        e.target.src = imagen_ncognito;
+        refFondo.current.backgroundColor = "white";
+    }
+
     return (
         <div className="container"> 
-            <div className="fondo">            
-                <img src = {imagen_1} className = "rey" alt="condorito"/>
+            <div ref={refFondo} className="fondo">            
+                <img onClick={incognito} src = {imagen_1} className = "rey" alt="condorito"/>
                 <div>Sisebuto</div>
             </div>
 
             <div className="fondo">
-                <img src = {imagen_2} className = "rey" alt="condorito"/>
+                <img onClick={incognito} src = {imagen_2} className = "rey" alt="condorito"/>
                 <div>Leogivildo</div>
             </div>
 
             <div className="fondo">
-                <img src = {imagen_3} className = "rey" alt="condorito"/>
+                <img onClick={incognito} src = {imagen_3} className = "rey" alt="condorito"/>
                 <div>Atanagildo</div>
             </div>
         </div>
